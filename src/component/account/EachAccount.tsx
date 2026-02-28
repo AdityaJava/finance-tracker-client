0cdae9734b720d434cdaf986b4b1f3b81cd8ac81
Aditya <adityadudhal97@gmail.com>
2026-02-28 09:39:05 +0530

    feat: refactor EachAccount component to simplify editing logic and add EditAccount component

--- src/component/account/EachAccount.tsx
+++ src/component/account/EachAccount.tsx
@@ -11,57 +11,53 @@ export default function EachAccount({ element, loadAccounts }: AccountProps) {
         await deleteAccountById(elementId);
         loadAccounts(0, 10)
     }
-
-    const [editingMap, setEditingMap] = useState<Record<number, boolean>>();
-
+    const [editMode, setEditMode] = useState<boolean>(false);
 
     const editing = (elementId: number) => {
-        setEditingMap(prev => ({
-            ...prev,
-            [elementId]: true
-        }));
+        setEditMode(true);
     }
 
     return (
-        <div
-            key={element.id}
-            className="bg-white rounded-2xl shadow-md hover:shadow-lg transition-shadow duration-300 p-5 border border-gray-100"
-        >
-            <div className="flex justify-between items-center mb-3">
-                <h2 className="text-lg font-semibold text-gray-800" onClick={() => editing(element.id)}>
-                    {element.name}
-                </h2>
-                <span
-                    className={`text-xs px-3 py-1 rounded-full font-medium ${element.active
-                        ? "bg-green-100 text-green-700"
-                        : "bg-red-100 text-red-600"
-                        }`}
-                >
-                    {element.active ? "Active" : "Inactive"}
-                </span>
-            </div>
+        (!editMode ?
+            <div
+                key={element.id}
+                className="bg-white rounded-2xl shadow-md hover:shadow-lg transition-shadow duration-300 p-5 border border-gray-100"
+            >
+                <div className="flex justify-between items-center mb-3">
+                    <h2 className="text-lg font-semibold text-gray-800" onClick={() => editing(element.id)}>
+                        {element.name}
+                    </h2>
+                    <span
+                        className={`text-xs px-3 py-1 rounded-full font-medium ${element.active
+                            ? "bg-green-100 text-green-700"
+                            : "bg-red-100 text-red-600"
+                            }`}
+                    >
+                        {element.active ? "Active" : "Inactive"}
+                    </span>
+                </div>
 
-            <div className="space-y-2 text-sm text-gray-600">
-                <p>
-                    <span className="font-medium text-gray-700">
-                        Type:
-                    </span>{" "}
-                    {element.type}
-                </p>
-                <p>
-                    <span className="font-medium text-gray-700">
-                        Opening Balance:
-                    </span>{" "}
-                    ₹{element.openingBalance}
-                </p>
-            </div>
-            <div className="m-2 ml-0">
-                <button onClick={() => deleteAccount(element.id)} className="p-2 rounded-lg hover:bg-red-100 transition-colors duration-200"
-                >
-                    <img src={deleteIcon} className="w-5 h-5"
-                        alt="delete" />
-                </button>
-            </div>
-        </div>
+                <div className="space-y-2 text-sm text-gray-600">
+                    <p>
+                        <span className="font-medium text-gray-700">
+                            Type:
+                        </span>{" "}
+                        {element.type}
+                    </p>
+                    <p>
+                        <span className="font-medium text-gray-700">
+                            Opening Balance:
+                        </span>{" "}
+                        ₹{element.openingBalance}
+                    </p>
+                </div>
+                <div className="m-2 ml-0">
+                    <button onClick={() => deleteAccount(element.id)} className="p-2 rounded-lg hover:bg-red-100 transition-colors duration-200"
+                    >
+                        <img src={deleteIcon} className="w-5 h-5"
+                            alt="delete" />
+                    </button>
+                </div>
+            </div> : <>edit</>)
     )
 }
---
+++ src/component/account/EditAccount.tsx
@@ -0,0 +1,9 @@
+import type { JSX } from "react";
+
+export function EditAccount(): JSX.Element {
+    return (
+        <div>
+
+        </div>
+    )
+}
